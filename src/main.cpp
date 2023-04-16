cf7529e8d09d9c8f668679194bdb17478c5bdd93
Riley Cornelius <rileyccornelius@gmail.com>
2023-04-16 11:06:37 -0500

    Test display and touch drivers

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#include <ESP32_S3_Box_TouchScreen.h>
 
-void setup() {
-  // put your setup code here, to run once:
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+ESP32S3BOX_TS ts = ESP32S3BOX_TS();
+
+void setup(void)
+{
+  Serial.begin(115200);
+  ts.begin();
+  tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.drawString("Hello World", 0, 0, 2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  uint16_t x, y;
+  if (ts.touched())
+  {
+    // Retrieve a point
+    TS_Point p = ts.getPoint();
+    Serial.println("X = " + String(p.x) + " Y = " + String(p.y));
+  }
 }
