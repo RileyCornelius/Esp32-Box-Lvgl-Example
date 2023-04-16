61ed606aa6b394f8de882acfee22be12295a39b0
Riley Cornelius <rileyccornelius@gmail.com>
2023-04-16 12:48:12 -0500

    Fix issues with lv_setup and increase display buffer size

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <ui/lv_setup.h>
+#include <ui/ui.h>
 
 void setup(void)
 {
+  Serial.begin(115200);
   lv_begin();
+  ui_init();
 }
 
 void loop()
--- src/ui/lv_setup.cpp
+++ src/ui/lv_setup.cpp
@@ -5,13 +5,11 @@
 #include <ESP32_S3_Box_TouchScreen.h>
 
 /* Esp32-box screen size */
-static const uint16_t screenWidth = 320;
-static const uint16_t screenHeight = 240;
+#define LV_SCREEN_WIDTH 320
+#define LV_SCREEN_HEIGHT 240
+#define LV_BUF_SIZE (LV_SCREEN_WIDTH * LV_SCREEN_HEIGHT)
 
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[screenWidth * screenHeight / 10];
-
-TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight);
+TFT_eSPI tft = TFT_eSPI(LV_SCREEN_WIDTH, LV_SCREEN_HEIGHT);
 ESP32S3BOX_TS ts = ESP32S3BOX_TS();
 
 /* Display flushing */
@@ -22,20 +20,20 @@ static void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t
 
     tft.setAddrWindow(area->x1, area->y1, w, h);
     tft.pushColors((uint16_t *)&color_p->full, w * h);
+
     lv_disp_flush_ready(disp);
 }
 
 /* Read the touchpad */
 static void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
-    uint16_t x, y;
     if (ts.touched())
     {
         TS_Point p = ts.getPoint(); // Retrieve a point
-        data->point.x = x;
-        data->point.y = y;
+        data->point.x = p.x;
+        data->point.y = p.y;
         data->state = LV_INDEV_STATE_PR;
-        // Serial.println("X = " + String(p.x) + " Y = " + String(p.y));
+        // Serial.println("X = " + String(p.x) + " Y = " + String(p.y) + " Pressure = " + String(p.p));
     }
     else
     {
@@ -46,17 +44,30 @@ static void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data
 /* Setup lvgl with display and touch pad */
 void lv_begin()
 {
-    lv_init(); // call this before any other lvgl function
+    /*Initialize the display driver*/
+    tft.init();
+    tft.setRotation(3);
 
-    lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * screenHeight / 10); // initialize the display buffer
+    /*Initialize the touch driver*/
+    ts.begin();
+
+    /* Call before other lv functions */
+    lv_init();
+
+    /* Initialize the display buffers */
+    static lv_disp_draw_buf_t draw_buf;
+    static lv_color_t *buf1, *buf2;
+    buf1 = (lv_color_t *)heap_caps_malloc(LV_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM);
+    assert(buf1);
+    buf2 = (lv_color_t *)heap_caps_malloc(LV_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM);
+    assert(buf2);
+    lv_disp_draw_buf_init(&draw_buf, buf1, buf2, LV_BUF_SIZE);
 
     /*Initialize the display*/
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-
-    /*Change the following line to your display resolution*/
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
+    disp_drv.hor_res = LV_SCREEN_WIDTH;
+    disp_drv.ver_res = LV_SCREEN_HEIGHT;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register(&disp_drv);
@@ -67,8 +78,6 @@ void lv_begin()
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = my_touchpad_read;
     lv_indev_drv_register(&indev_drv);
-
-    Serial.printf("LVGL v%d.%d.%d initialized\n", lv_version_major(), lv_version_minor(), lv_version_patch());
 }
 
 /* Handles updating the display and touch events */
@@ -81,5 +90,6 @@ void lv_handler()
     {
         previousUpdate = millis();
         uint32_t interval = lv_timer_handler(); // Update the UI
+        // Serial.println(interval);
     }
 }
